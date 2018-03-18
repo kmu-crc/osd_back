f7430c24ebd2ff6225922c5cdfe774aa0f164823
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-03-18 22:44:22 +0900

    eslint설정

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "indent": [
+            "warn",
+            2
+          ],
+        "semi": ["warn", "always"],
+        "quotes": ["error", "double"],
+        "no-unused-vars": 1,
+        "no-unreachable": ["warn"]
+    }
+};
