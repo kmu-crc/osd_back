5fb75d92a04fb2bad27a89ff37b12665e453ccca
염서연 <sy8095@gmail.com>
2018-03-22 15:53:32 +0900

    종훈이 코멘트 추가

--- routes/design/index.js
+++ routes/design/index.js
@@ -7,6 +7,7 @@ var { designIssue } = require("./designIssue");
 
 router.get("/designList", designList);
 router.get("/designList/:level/:category", designList);
+// designList?level=2&category=1 이렇게 보내고 뒤에서 req.query 이렇게 받으면 된대!
 router.get("/designDetail/:id", designDetail);
 router.get("/designDetail/:id/step", designStep);
 router.get("/designDetail/:id/createBoard", createBoard);
