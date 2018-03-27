67cfcde7c1e3ab0dd31a6a0b70287d5e6c259335
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-03-27 17:30:08 +0900

    /files router 추가

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ var express = require("express");
 var router = express.Router();
 var users = require("./users");
 var design = require("./design");
+var files = require("./files");
 
 /* GET home page. */
 router.use("/users", users);
 router.use("/design", design);
+router.use("/files", files);
 
 module.exports = router;
