240d7dbe4f2bd399e125bad761ab384f6decd8d5
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-03-28 01:15:20 +0900

    회원가입 완료시 자동으로 로그인 정보를 반환하도록 middleware 추가

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,19 +1,18 @@
 var express = require("express");
 var router = express.Router();
-var { signUp } = require("./signUp");
-var { basicSignIn } = require("./basicSignIn");
+var signUp = require("./signUp");
+var signIn = require("./signIn");
 var check = require("./check");
 var auth = require("../../middlewares/auth");
-var FBSignUp = require("./FBSignUp");
-var FBSignIn = require("./FBSignIn");
+var { FBSignIn, FBSignUp } = require("./FBRegistration");
 
-router.post("/signUp", signUp);
-router.post("/basicSignIn", basicSignIn);
+router.post("/signUp", signUp, signIn);
+router.post("/signIn", signIn);
 
 router.use("/check", auth, check);
 
-router.post("/FBSignUp", FBSignUp);
+router.post("/FBSignUp", FBSignUp, FBSignIn);
 
-router.post("/FBSignIn", FBSignIn);
+router.post("/FBSignIn", FBSignIn, FBSignUp, FBSignIn);
 
 module.exports = router;
