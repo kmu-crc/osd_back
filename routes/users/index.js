715aaafe0ccb133d6a1af9b81d8192fe71dcbb1d
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-03-28 17:49:23 +0900

    userDetail router 추가, var -> const

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,10 +1,26 @@
-var express = require("express");
-var router = express.Router();
-var signUp = require("./signUp");
-var signIn = require("./signIn");
-var check = require("./check");
-var auth = require("../../middlewares/auth");
-var { FBSignIn, FBSignUp } = require("./FBRegistration");
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const signUp = require("./signUp");
+const signIn = require("./signIn");
+const check = require("./check");
+const auth = require("../../middlewares/auth");
+const { FBSignIn, FBSignUp } = require("./FBRegistration");
+const { insertDetail } = require("./userDetail");
+const stringToNumber = require("../../middlewares/stringToNumber");
+
+// thumbnail 만드는데 사용되는 middleware
+const multer = require("multer"); // express에 multer모듈 적용 (for 파일업로드);
+const createThumbnail = require("../../middlewares/createThumbnail");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().valueOf() + path.extname(file.originalname)); // cb 콜백함수를 통해 전송된 파일 이름 설정
+  }
+});
+const upload = multer({ storage: storage });
 
 router.post("/signUp", signUp, signIn);
 router.post("/signIn", signIn);
@@ -14,5 +30,7 @@ router.use("/check", auth, check);
 router.post("/FBSignUp", FBSignUp, FBSignIn);
 
 router.post("/FBSignIn", FBSignIn, FBSignUp, FBSignIn);
+
+router.post("/insertDetail", auth, upload.single("thumbnail"), createThumbnail, stringToNumber, insertDetail);
 
 module.exports = router;
