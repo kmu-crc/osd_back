9db3f7044bac66a1861a0dfb8bf0958e066933f7
염서연 <sy8095@gmail.com>
2018-03-30 00:39:18 +0900

    디자인리스트, 그룹리스트에 promise.all 반영함! 테스트 완료!

--- routes/group/groupList.js
+++ routes/group/groupList.js
@@ -1,16 +1,29 @@
 var connection = require("../../configs/connection");
-const { getDesignTop3 } = require("../../middlewares/getDesignTop3");
+// const { getDesignTop3 } = require("../../middlewares/getDesignTop3");
 
+// 그룹 리스트 가져오기 (GET)
 exports.groupList = (req, res, next) => {
-  // 그룹 리스트 가져오기 (GET)
   function getGroupList () {
     const p = new Promise((resolve, reject) => {
-      connection.query("SELECT * FROM group", (err, row) => {
+      let arr = [];
+      connection.query("SELECT * FROM opendesign.group", (err, row) => {
         if (!err) {
           for (var i = 0, l = row.length; i < l; i++) {
-            const groupData = row[i];
-            resolve(groupData);
+            let groupData = row[i];
+            arr.push(new Promise((resolve, reject) => {
+              connection.query("SELECT * FROM group_counter WHERE group_id = ?", groupData.uid, (err, result) => {
+                if (!err) {
+                  groupData.count = result[0];
+                  resolve(groupData);
+                } else {
+                  reject(err);
+                }
+              });
+            }));
           }
+          Promise.all(arr).then(result => {
+            resolve(result);
+          }).catch(console.log("no"));
         } else {
           reject(err);
         }
@@ -20,23 +33,22 @@ exports.groupList = (req, res, next) => {
   };
 
   // 그룹 좋아요 수, 멤버수, 디자인수, 총 좋아요 수 가져오기 (GET)
-  function getGroupCount (data) {
-    const p = new Promise((resolve, reject) => {
-      const groupId = data.uid;
-      connection.query("SELECT * FROM group_counter WHERE uid = ?", groupId, (err, result) => {
-        if (!err) {
-          data.count = result[0];
-          resolve(data);
-        } else {
-          reject(err);
-        }
-      });
-    });
-    return p;
-  };
+  // function getGroupCount (data) {
+  //   const p = new Promise((resolve, reject) => {
+  //     const groupId = data.uid;
+  //     connection.query("SELECT * FROM group_counter WHERE uid = ?", groupId, (err, result) => {
+  //       if (!err) {
+  //         data.count = result[0];
+  //         console.log("count working");
+  //         resolve(data);
+  //       } else {
+  //         reject(err);
+  //       }
+  //     });
+  //   });
+  //   return p;
+  // };
 
   getGroupList()
-    .then(getGroupCount)
-    .then(data => getDesignTop3(data, "groupId"))
     .then(data => res.json(data));
 };
