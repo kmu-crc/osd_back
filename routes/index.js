5c84130608bcda332987f8e076fd556eaf3861df
염서연 <sy8095@gmail.com>
2018-03-30 16:13:02 +0900

    디자이너 리스트 get 요청 완료

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var users = require("./users");
 var design = require("./design");
 var group = require("./group");
+var designer = require("./designer");
 var admins = require("./admins");
 
 /* GET home page. */
@@ -10,5 +11,6 @@ router.use("/users", users);
 router.use("/design", design);
 router.use("/admins", admins);
 router.use("/group", group);
+router.use("/designer", designer);
 
 module.exports = router;
