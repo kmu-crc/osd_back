fca6f3549881186cc5fb46b7ecafc6ff07c8a995
염서연 <sy8095@gmail.com>
2018-03-30 10:30:14 +0900

    step 탭에서 보드 & 카드 가져오는 기능에 promise.all 반영!

--- routes/design/designStep.js
+++ routes/design/designStep.js
@@ -3,49 +3,32 @@ var connection = require("../../configs/connection");
 // 디자인 스텝 보드 가져오기 (GET)
 exports.designStep = (req, res, next) => {
   const designId = req.params.id;
-  let arr = [];
 
   // board 목록 가져오기
   function getBoardList (id) {
     const p = new Promise((resolve, reject) => {
+      let arr = [];
       connection.query("SELECT * FROM design_board WHERE design_id = ?", id, (err, row) => {
         if (!err) {
           for (var i = 0, l = row.length; i < l; i++) {
-            let boardData = row[i];
-            resolve(boardData);
+            arr.push(new Promise((resolve, reject) => {
+              let boardData = row[i];
+              let sql = "SELECT D.uid, D.user_id, U.nick_name, D.first_img, D.title, D.order, D.is_complete_card, D.update_time, C.comment_count FROM design_card D JOIN card_counter C ON D.uid = C.card_id JOIN user U ON D.user_id = U.uid WHERE board_id = ?";
+              if (!err) {
+                connection.query(sql, boardData.uid, (err, row) => {
+                  if (!err) {
+                    boardData.cardData = row;
+                    resolve(boardData);
+                  } else {
+                    reject(err);
+                  }
+                });
+              }
+            }));
           }
-        } else {
-          reject(err);
-        }
-      });
-    });
-    return p;
-  };
-
-  // 해당 board에 속한 card 가져오기
-  function getCardList (data) {
-    const p = new Promise((resolve, reject) => {
-      const boardId = data.uid;
-      connection.query("SELECT * FROM design_card WHERE board_id = ?", boardId, (err, row) => {
-        if (!err) {
-          data.cardData = row[0];
-          resolve(data);
-        } else {
-          reject(err);
-        }
-      });
-    });
-    return p;
-  };
-
-  // board 안에 있는 각 card들의 댓글 수 가져오기
-  function getCardCount (data) {
-    const p = new Promise((resolve, reject) => {
-      const cardId = data.cardData.uid;
-      connection.query("SELECT comment_count FROM card_counter WHERE card_id = ?", cardId, (err, result) => {
-        if (!err) {
-          data.cardData.cardCount = result[0];
-          resolve(data);
+          Promise.all(arr).then(result => {
+            resolve(result);
+          });
         } else {
           reject(err);
         }
@@ -55,10 +38,7 @@ exports.designStep = (req, res, next) => {
   };
 
   getBoardList(designId)
-    .then(getCardList)
-    .then(getCardCount)
-    .then(data => arr.push(data))
-    .then(arr => res.status(200).json(arr));
+    .then(data => res.json(data));
 };
 
 // **********************************************************
