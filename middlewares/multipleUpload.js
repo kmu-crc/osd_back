79a1def0b4f3a85d3d1a469e8db0fc0bb685da3e
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-05-15 16:58:21 +0900

    파일 업로드 middlewares 생성

---
+++ middlewares/multipleUpload.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const multer = require("multer"); // express에 multer모듈 적용 (for 파일업로드);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().valueOf() + path.extname(file.originalname)); // cb 콜백함수를 통해 전송된 파일 이름 설정
+  }
+});
+const upload = multer({ storage: storage });
+
+const multipleUpload = (req, res, next) => {
+  const thisUpload = upload.fields([{name: "thumbnail", maxCount: 1}, {name: "designs[]"}]);
+  thisUpload(req, res, (err) => {
+    if (err) {
+      console.log(err);
+      next(err);
+    }
+    if (req.file) {
+      next();
+    } else {
+      req.file = null;
+      next();
+    }
+  });
+};
+
+module.exports = multipleUpload;
