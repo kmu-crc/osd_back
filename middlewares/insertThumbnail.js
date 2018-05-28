4f15ab9ec368a1f1550220fd034a823e58233897
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-05-28 18:52:42 +0900

    thumbnail파일이 req에 포암되어있다면 파일과 데이터를 분리하는 로직

---
+++ middlewares/insertThumbnail.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const multer = require("multer"); // express에 multer모듈 적용 (for 파일업로드);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().valueOf() + path.extname(file.originalname)); // cb 콜백함수를 통해 전송된 파일 이름 설정
+  }
+});
+const upload = multer({ storage: storage });
+
+const imageUpload = (req, res, next) => {
+  const thisUpload = upload.single("thumbnail");
+  thisUpload(req, res, (err) => {
+    if (err) {
+      next(err);
+    }
+    if (req.file) {
+      next();
+    } else {
+      req.file = null;
+      next();
+    }
+  });
+};
+
+module.exports = imageUpload;
