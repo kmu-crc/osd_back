4ca85be0426192d440a1e3402e9377c735e513f2
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-05-28 18:54:11 +0900

    디자인을 등록하기 위해 넘어오는 req에서 file을 분리하는 로직

--- middlewares/uploadDesign.js
+++ middlewares/uploadDesign.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const multer = require("multer"); // express에 multer모듈 적용 (for 파일업로드);
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads/"); // cb 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
@@ -8,19 +9,19 @@ const storage = multer.diskStorage({
     cb(null, new Date().valueOf() + path.extname(file.originalname)); // cb 콜백함수를 통해 전송된 파일 이름 설정
   }
 });
+
 const upload = multer({ storage: storage });
 
 const uploadDesign = (req, res, next) => {
   const thisUpload = upload.fields([{name: "thumbnail", maxCount: 1}, {name: "design_file[]"}, {name: "source_file[]"}]);
   thisUpload(req, res, (err) => {
     if (err) {
-      console.log(err);
       next(err);
     }
-    if (req.file) {
+    if (req.files) {
       next();
     } else {
-      req.file = null;
+      req.files = null;
       next();
     }
   });
