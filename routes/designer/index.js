f3f025bef9f440ed6eca60786a5ff2fde56fcfb7
seoyeonyeom <sy8095@gmail.com>
2018-05-28 22:43:27 +0900

    디자이너 sorting, 오타 수정

--- routes/designer/index.js
+++ routes/designer/index.js
@@ -7,7 +7,7 @@ const getDesignerList = require("../../middlewares/getDesignerList");
 const { designInDesigner, likeInDesigner } = require("./designInDesigner");
 const getDesignList = require("../../middlewares/getDesignList");
 
-router.get("/designerList", designerList, getDesignerList);
+router.get("/designerList/:page/:sorting?/:cate1?/:cate2?", designerList, getDesignerList);
 router.get("/designerDetail/:id", designerDetail);
 router.get("/designerDetail/:id/design", designInDesigner, getDesignList);
 router.get("/designerDetail/:id/like", likeInDesigner, getDesignList);
