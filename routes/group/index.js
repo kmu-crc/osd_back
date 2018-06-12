06e373c468ea2a9ffc1b520568fdd9eab904b84e
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-12 18:13:36 +0900

    디자인 그룹가입, 그룹 그룹가입, 그룹생성 router 추가

--- routes/group/index.js
+++ routes/group/index.js
@@ -10,17 +10,22 @@ const { groupList } = require("./groupList");
 const { groupDetail } = require("./groupDetail");
 const { designInGroup } = require("./designInGroup");
 const { groupInGroup } = require("./groupInGroup");
-const groupSignUp = require("./groupSignUp");
+const { groupSignUp, groupSignUpGroup } = require("./groupSignUp");
 const { createGroup } = require("./createGroup");
 const { waitingDesign, waitingGroup } = require("./waitingList");
+const insertThumbnail = require("../../middlewares/insertThumbnail");
+const { myDesignList, myGroupList } = require("./getMyList");
 
 router.get("/groupList/:page/:sorting?", groupList, getGroupList);
 router.get("/groupDetail/:id", groupDetail);
 router.get("/groupDetail/:id/design/:page/:sorting?", designInGroup, getDesignList);
 router.get("/groupDetail/:id/group/:page/:sorting?", groupInGroup, getGroupList);
-router.post("/groupSignUp", auth, groupSignUp);
+router.get("/:id/join/myDesignList", auth, myDesignList);
+router.get("/:id/join/myGroupList", auth, myGroupList);
+router.post("/groupDetail/:id/DesignJoinGroup", auth, groupSignUp);
+router.post("/groupDetail/:id/GroupJoinGroup", auth, groupSignUpGroup);
 router.get("/groupDetail/:id/waitingDesign", waitingDesign, getDesignList);
 router.get("/groupDetail/:id/waitingGroup", waitingGroup, getGroupList);
-// router.post("/createGroup", auth, createThumbnail, stringToNumber, createGroup);
+router.post("/createGroup", auth, insertThumbnail, stringToNumber, createGroup);
 
 module.exports = router;
