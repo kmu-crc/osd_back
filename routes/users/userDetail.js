1456e01b0fda15f027c69676ea9534895bc1abf6
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-24 02:05:08 +0900

    category값이 0으로 넘어오는경우 db에 null이 들어가게 변경

--- routes/users/userDetail.js
+++ routes/users/userDetail.js
@@ -7,25 +7,35 @@ const { isOnlyNicName } = require("../../middlewares/verifications");
 exports.insertDetail = (req, res) => {
   console.log("insert", req.file);
   req.body["user_id"] = req.decoded.uid;
+  if (req.body.category_level1 === 0) {
+    req.body.category_level1 = null;
+  }
+  if (req.body.category_level2 === 0) {
+    req.body.category_level2 = null;
+  }
   if (req.body.is_designer) {
     req.body.is_designer = 1;
   } else {
     req.body.is_designer = 0;
   }
-  const userUpdata = (id) => {
+  const userUpdata = id => {
     return new Promise((resolve, reject) => {
-      connection.query(`UPDATE user SET ? WHERE uid = ${req.decoded.uid} `, {thumbnail: id}, (err, rows) => {
-        if (!err) {
-          console.log("detail: ", rows);
-          resolve(rows);
-        } else {
-          reject(err);
+      connection.query(
+        `UPDATE user SET ? WHERE uid = ${req.decoded.uid} `,
+        { thumbnail: id },
+        (err, rows) => {
+          if (!err) {
+            console.log("detail: ", rows);
+            resolve(rows);
+          } else {
+            reject(err);
+          }
         }
-      });
+      );
     });
   };
 
-  const insertDetailDB = (data) => {
+  const insertDetailDB = data => {
     console.log("22", data);
     return new Promise((resolve, reject) => {
       connection.query("INSERT INTO user_detail SET ?", data, (err, rows) => {
@@ -41,25 +51,35 @@ exports.insertDetail = (req, res) => {
 
   const insertUserCount = () => {
     return new Promise((resolve, reject) => {
-      const newCount = { user_id: req.decoded.uid, total_like: 0, total_design: 0, total_group: 0, total_view: 0 };
-      connection.query("INSERT INTO user_counter SET ? ", newCount, (err, row) => {
-        if (!err) {
-          resolve(row);
-        } else {
-          console.log(err);
-          reject(err);
+      const newCount = {
+        user_id: req.decoded.uid,
+        total_like: 0,
+        total_design: 0,
+        total_group: 0,
+        total_view: 0
+      };
+      connection.query(
+        "INSERT INTO user_counter SET ? ",
+        newCount,
+        (err, row) => {
+          if (!err) {
+            resolve(row);
+          } else {
+            console.log(err);
+            reject(err);
+          }
         }
-      });
+      );
     });
   };
 
-  const respond = (data) => {
+  const respond = data => {
     res.status(200).json({
       message: "성공적으로 등록되었습니다."
     });
   };
 
-  const error = (err) => {
+  const error = err => {
     res.status(500).json({
       error: err
     });
@@ -88,29 +108,33 @@ exports.modifyDetail = (req, res) => {
     is_designer: req.body.is_designer
   }; // user detail 테이블에 들어가야 할 정보
 
-  const updateDetailDB = (data) => {
+  const updateDetailDB = data => {
     console.log(data);
     return new Promise((resolve, reject) => {
-      connection.query(`UPDATE user_detail SET ? WHERE user_id=${userId}`, data, (err, rows) => {
-        if (!err) {
-          if (rows.affectedRows) {
-            resolve(rows);
+      connection.query(
+        `UPDATE user_detail SET ? WHERE user_id=${userId}`,
+        data,
+        (err, rows) => {
+          if (!err) {
+            if (rows.affectedRows) {
+              resolve(rows);
+            } else {
+              console.log(err);
+              reject(err);
+            }
           } else {
             console.log(err);
             reject(err);
           }
-        } else {
-          console.log(err);
-          reject(err);
         }
-      });
+      );
     });
   };
 
-  function createHashPw (userInfo) {
+  function createHashPw(userInfo) {
     console.log(userInfo);
     const p = new Promise((resolve, reject) => {
-      bcrypt.hash(userInfo.password, 10, function (err, hash) {
+      bcrypt.hash(userInfo.password, 10, function(err, hash) {
         if (!err) {
           userInfo.password = hash;
           console.log("createHashPW", userInfo);
@@ -122,32 +146,36 @@ exports.modifyDetail = (req, res) => {
       });
     });
     return p;
-  };
+  }
 
-  const userUpdata = (id) => {
+  const userUpdata = id => {
     let info = userInfo;
     info.thumbnail = id;
     return new Promise((resolve, reject) => {
-      connection.query(`UPDATE user SET ? WHERE uid = ${req.decoded.uid}`, info, (err, rows) => {
-        if (!err) {
-          console.log("detail: ", rows);
-          resolve(rows);
-        } else {
-          console.log(err);
-          reject(err);
+      connection.query(
+        `UPDATE user SET ? WHERE uid = ${req.decoded.uid}`,
+        info,
+        (err, rows) => {
+          if (!err) {
+            console.log("detail: ", rows);
+            resolve(rows);
+          } else {
+            console.log(err);
+            reject(err);
+          }
         }
-      });
+      );
     });
   };
 
-  const respond = (data) => {
+  const respond = data => {
     res.status(200).json({
       success: true,
       message: "성공적으로 업데이트되었습니다."
     });
   };
 
-  const error = (err) => {
+  const error = err => {
     res.status(500).json({
       success: false,
       error: err
