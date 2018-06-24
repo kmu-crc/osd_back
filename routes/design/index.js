36608eda7bf967a4b09a182df2bcd111bde7dd04
jonghun kwon <jonghunkwon@jonghunui-MacBook-Pro.local>
2018-06-24 16:16:44 +0900

    안쓰는 updatecard 제거

--- routes/design/index.js
+++ routes/design/index.js
@@ -15,7 +15,7 @@ const { createDesign } = require("./createDesign");
 const uploadDesign = require("../../middlewares/uploadDesign");
 const stringToNumber = require("../../middlewares/stringToNumber");
 const { createBoard, getBoardList, updateBoard, deleteBoard } = require("./designBoard");
-const { createCard, getCardList, updateCard, updateTitle, updateContent, getCardDetail, updateImages, updateSources } = require("./designCard");
+const { createCard, getCardList, updateTitle, updateContent, getCardDetail, updateImages, updateSources } = require("./designCard");
 
 router.get("/designList/:page/:sorting?/:cate1?/:cate2?", designList, getDesignList);
 router.get("/designDetail/:id", tokenDecoded, designDetail);
@@ -37,13 +37,12 @@ router.post("/updateViewCount/:id", updateViewCount);
 router.post("/createDesign", auth, uploadDesign, stringToNumber, createDesign);
 router.post("/designDetail/:id/createBoard", auth, stringToNumber, createBoard);
 router.post("/designDetail/:id/:boardId/createCard", auth, stringToNumber, createCard);
-router.post("/designDetail/:id/:boardId/updateCard/:cardId", auth, uploadDesign, stringToNumber, updateCard);
 router.post("/designDetail/updateBoard/:board_id", auth, updateBoard);
 router.post("/designDetail/updateCardTitle/:cardId", auth, stringToNumber, updateTitle);
 router.post("/designDetail/updateCardContent/:cardId", auth, stringToNumber, updateContent);
 router.post("/designDetail/updateCardImages/:cardId", auth, uploadDesign, stringToNumber, updateImages);
 router.post("/designDetail/updateCardSources/:cardId", auth, uploadDesign, stringToNumber, updateSources);
-router.delete("/designDetail/deleteBoard/:board_id", auth, deleteBoard);
+router.delete("/designDetail/:design_id/deleteBoard/:board_id", auth, deleteBoard);
 
 router.get("/designDetail/:id/issue", designIssue);
 router.get("/designDetail/:id/issueDetail/:issue_id", designIssueDetail);
