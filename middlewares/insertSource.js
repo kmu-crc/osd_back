57f32393f18fa6185bdcb2a7a77ec04e281626fb
seoyeonyeom <sy8095@gmail.com>
2018-07-01 18:16:14 +0900

    디자인 수정할때 이미지 & 파일 업로드 기다렷다가 새로고침 하도록 수정

--- middlewares/insertSource.js
+++ middlewares/insertSource.js
@@ -1,46 +1,47 @@
 const connection = require("../configs/connection");
 const { S3Sources } = require("../middlewares/S3Sources");
 
-exports.insertSource = (data) => {
-  return new Promise((resolve, reject) => {
-    let type = null;
-    if (data.tabel === "design_source_file") {
-      type = "sources";
-    } else if (data.tabel === "design_images") {
-      type = "images";
-    }
-    if (!data.files) resolve(null);
+exports.insertSource = async data => {
+  let type = null;
+  if (data.tabel === "design_source_file") {
+    type = "sources";
+  } else if (data.tabel === "design_images") {
+    type = "images";
+  }
+  if (!data.files) Promise.resolve(null);
 
-    const insertDB = (obj) => {
-      return new Promise((resolve, reject) => {
-        connection.query(`INSERT INTO ${data.tabel} SET ?`, obj, (err, rows) => {
-          if (!err) {
-            resolve(rows);
-          } else {
-            reject(err);
-          }
-        });
+  const insertDB = obj => {
+    return new Promise((resolve, reject) => {
+      connection.query(`INSERT INTO ${data.tabel} SET ?`, obj, (err, rows) => {
+        if (!err) {
+          resolve(rows);
+        } else {
+          reject(err);
+        }
       });
+    });
+  };
+
+  let PromiseArray = [];
+
+  console.log("data", data);
+  for (let item of data.files) {
+    let obj = {
+      user_id: data.uid,
+      card_id: data.card_id,
+      name: item.originalname,
+      link: null,
+      type: item.mimetype
     };
-
-    let PromiseArray = data.files.map((item) => {
-      return new Promise((resolve, reject) => {
-        let obj = {
-          user_id: data.uid,
-          card_id: data.card_id,
-          name: item.originalname,
-          link: null,
-          type: item.mimetype
-        };
-        S3Sources({file: item, type}).then(path => {
-          obj.link = path;
-          return insertDB(obj);
-        });
-      });
+    await S3Sources({ file: item, type }).then(path => {
+      obj.link = path;
+      return insertDB(obj).then(PromiseArray.push(true)).catch(err => PromiseArray.push(err));
     });
-    console.log(PromiseArray);
-    return Promise.all(PromiseArray).then(resolve(true)).catch((err) => {
-      reject(err);
+  }
+  console.log(PromiseArray);
+  return Promise.all(PromiseArray)
+    .then(Promise.resolve(true))
+    .catch(err => {
+      Promise.reject(err);
     });
-  });
 };
