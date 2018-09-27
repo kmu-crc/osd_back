2417115b79b94c8a95deb30888f38a6825b091d1
seoyeonyeom <sy8095@gmail.com>
2018-09-27 19:29:49 +0900

    서치 멤버에 디자인 id 넘김

--- routes/search/index.js
+++ routes/search/index.js
@@ -5,7 +5,7 @@ const auth = require("../../middlewares/auth");
 const searchMembers = require("./searchMembers");
 const { searchIssue } = require("./searchIssue");
 
-router.post("/members", auth, searchMembers);
+router.post("/members/:id?", auth, searchMembers);
 router.get("/:id/designIssue/:keyword", searchIssue);
 
 module.exports = router;
