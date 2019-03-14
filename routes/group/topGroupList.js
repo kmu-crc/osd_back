83cfe969339bcda987df963fac92a586af087a52
ggamsang <w.ggmsng@gmail.com>
2019-03-14 14:57:19 +0900

    #76 issue - modified query for present all designs top or not together, only this time

--- routes/group/topGroupList.js
+++ routes/group/topGroupList.js
@@ -1,43 +1,36 @@
 const connection = require("../../configs/connection");
 
 exports.topGroupList = (req, res, next) => {
-  const page = req.params.page;
-  let sort;
-  const keyword = req.params.keyword;
-
-  if (req.params.sorting !== "null" && req.params.sorting !== undefined && req.params.sorting !== "undefined") {
-    sort = req.params.sorting;
-  } else {
-    sort = "update";
-  }
-
+  const page = req.params.page; 
+  let sort; 
+  const keyword = req.params.keyword; 
+ 
+  if (req.params.sorting !== "null" && req.params.sorting !== undefined && req.params.sorting !== "undefined") { 
+    sort = req.params.sorting; 
+  } else { 
+    sort = "update"; 
+  } 
+ 
   let sql = `
-  SELECT
-    G.uid, G.title, G.thumbnail, G.create_time, G.child_update_time, G.user_id, G.explanation
-    , C.like, C.design, C.group
-    , U.nick_name
-    , CG.order
-    FROM opendesign.group G
-      LEFT JOIN opendesign.group_counter C ON C.group_id = G.uid
-      LEFT JOIN opendesign.user U ON U.uid = G.user_id
-      LEFT JOIN opendesign.collection_group CG ON CG.uid = G.uid
-    WHERE G.uid IN (
-      SELECT CG.group_id FROM opendesign.collection_group CG)
-    ORDER BY CG.order ASC, `;
-/*
-  if (keyword && keyword !== "null" && keyword !== "undefined") {
-    sql = sql + ` WHERE G.title LIKE "%${keyword}%" OR U.nick_name LIKE "%${keyword}%"`;
-  }
-*/
-  if (sort === "update") {
-    sql = sql + " G.child_update_time DESC LIMIT " + (page * 10) + ", 10";
-  } else if (sort === "create") {
-    sql = sql + " G.create_time DESC LIMIT " + (page * 10) + ", 10";
-  } else if (sort === "like") {
-    sql = sql + " C.like DESC LIMIT " + (page * 10) + ", 10";
-  }
-  req.sql = sql;
-  next();
+  SELECT T.uid, T.title, T.thumbnail, T.create_time, T.child_update_time, T.user_id, T.explanation, T.like, T.design, T.group, T.nick_name, T.order FROM (
+    SELECT G.uid,G.title,G.thumbnail,G.create_time,G.child_update_time ,G.user_id,G.explanation,C.like,C.design,C.group,U.nick_name,CG.order
+      FROM opendesign.group G
+        LEFT JOIN opendesign.group_counter C ON C.group_id = G.uid
+        LEFT JOIN opendesign.user U ON U.uid = G.user_id
+        LEFT JOIN opendesign.collection_group CG ON CG.group_id = G.uid
+      WHERE G.uid IN (SELECT CG.group_id FROM opendesign.collection_group CG)
+  UNION 
+    SELECT G.uid,G.title,G.thumbnail,G.create_time,G.child_update_time,G.user_id,G.explanation,C.like,C.design,C.group,U.nick_name,CG.order
+         FROM opendesign.group G
+        LEFT JOIN opendesign.group_counter C ON C.group_id = G.uid
+        LEFT JOIN opendesign.user U ON U.uid = G.user_id
+        LEFT JOIN opendesign.collection_group CG ON CG.group_id = G.uid
+      WHERE G.uid NOT IN (SELECT CG.group_id FROM opendesign.collection_group CG)        
+  ) as T 
+  order by T.order IS NULL ASC, T.order ASC, T.like DESC 
+  LIMIT `+(page*10)+`,10;`;
+  req.sql = sql; 
+  next(); 
 };
 
 exports.getTopTotalCount = (req, res, next) => {
