ddff3c523f6460c1630f89ca7fe6056164e5696f
ggamsang <w.ggmsng@gmail.com>
2019-05-21 19:30:19 +0900

    bug fix - sql order changed

--- routes/group/designInGroup.js
+++ routes/group/designInGroup.js
@@ -16,7 +16,7 @@ exports.designInGroup = (req, res, next) => {
 
   let sql = `SELECT
             D.uid, D.user_id, D.title, D.thumbnail, D.parent_design, D.category_level1, D.category_level2, D.create_time, D.update_time, D.is_public, D.is_project, 
-            C.like_count, C.member_count, C.card_count, C.view_count, F.children_count,
+            C.like_count, C.member_count, C.card_count, C.view_count, F.children_count
             FROM group_join_design G
               JOIN design D ON D.uid = G.design_id
               LEFT JOIN (SELECT DD.parent_design, COUNT(*) AS children_count FROM opendesign.design DD GROUP BY DD.parent_design) F ON F.parent_design = D.uid
